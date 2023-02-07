09e3d492d92c2e60e3d1d1fcc1c2e0ce4cd460d5
Raghav <raghav.surya@outlook.com>
2023-02-07 20:18:22 +0000

    Create account endpoint created

--- real_deal_api/lib/real_deal_api_web/controllers/account_controller.ex
+++ real_deal_api/lib/real_deal_api_web/controllers/account_controller.ex
@@ -1,8 +1,8 @@
 defmodule RealDealApiWeb.AccountController do
   use RealDealApiWeb, :controller
 
-  alias RealDealApi.Accounts
-  alias RealDealApi.Accounts.Account
+  alias RealDealApi.{Accounts, Accounts.Account, Users, Users.User}
+  alias RealDealApiWeb.Auth.Guardian
 
   action_fallback RealDealApiWeb.FallbackController
 
@@ -12,11 +12,13 @@ defmodule RealDealApiWeb.AccountController do
   end
 
   def create(conn, %{"account" => account_params}) do
-    with {:ok, %Account{} = account} <- Accounts.create_account(account_params) do
+    with {:ok, %Account{} = account} <- Accounts.create_account(account_params),
+          {:ok, token, _claims} <- Guardian.encode_and_sign(account),
+          {:ok, %User{} = user} <- Users.create_user(account, account_params) do
       conn
       |> put_status(:created)
      # |> put_resp_header("location", Routes.account_path(conn, :show, account))
-      |> render("show.json", account: account)
+      |> render("account_token.json", %{account: account, token: token})
     end
   end
 
